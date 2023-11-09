4790fc798768cbc6ec2e271f9ba476e14df7ecbb
Crowdbotics <team@crowdbotics.com>
2023-11-09 13:09:38 +0000

    Initial commit

---
+++ backend/voda_demo_5_44440/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for voda_demo_5_44440 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "voda_demo_5_44440.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "voda-demo-5-44440",
+  url: "https://voda-demo-5-44440.botics.co",
+  api: "https://voda-demo-5-44440.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
